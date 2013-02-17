e1818309a29424160aea7b3c860cd4487ec4f8f9
Scott <scott@Saruman.local>
2013-02-17 00:32:03 -0500

    Adding source and test files

---
+++ instancevars.py
@@ -0,0 +1,30 @@
+from functools import wraps
+import inspect
+
+def instancevars(func):
+	"""
+	A function decorator that automatically creates instance variables from
+	function arguments. Arguments can be omitted by naming them with a 
+	trailing underscore. Names are retained on a one-to-one basis.
+	"""
+	names, varargs, keywargs, defaults = inspect.getargspec(func)
+	
+	@wraps(func)
+	def wrapper(self, *args, **kwargs):
+		argnames = names[1:]
+		# This works because kwargs must always be last and zip trucates to shortest list
+		allargs = zip(argnames, args) + kwargs.items()
+		
+		for name, arg in allargs:
+			if name[-1] != '_': 
+				setattr(self, name, arg)
+		
+		if len(argnames) > len(allargs) and defaults is not None:
+			for i in range(len(defaults)):
+				idx = -(i+1)
+				if not hasattr(self, names[idx]) and names[idx][-1] != '_':
+					setattr(self, names[idx], defaults[idx])
+		
+		func(self, *args, **kwargs)
+
+	return wrapper
---
+++ instancevars_tests.py
@@ -0,0 +1,58 @@
+import unittest
+from instancevars import *
+
+class BasicTestClass(object):
+	@instancevars
+	def __init__(self, arg1, arg2_, arg3_='456', _arg4='789'):
+		self.arg3 = arg3_
+
+class InheritTestClass(BasicTestClass):
+	@instancevars
+	def __init__(self, arg1_, arg2, _newarg, arg3_, arg4_='123'):
+		super(InheritTestClass, self).__init__(arg1_, arg2, arg3_, arg4_)
+
+class NoDefaultsTestClass(object):
+	@instancevars
+	def __init__(self, arg1_, arg2, _arg3, arg4):
+		pass
+
+class TestInstanceVars(unittest.TestCase):
+	def assertNotExist(self, func):
+		try:
+			func()
+		except AttributeError:
+			pass
+		else:
+			self.fail('AttributeError should have been thrown')
+
+	def test_basic(self):
+		obj = BasicTestClass(1, 2, 3)
+		assert obj.arg1 == 1
+		self.assertNotExist(lambda: obj.arg2_ == 2)
+		self.assertNotExist(lambda: obj.arg3_ == 3)
+		assert obj.arg3 == 3
+		assert obj._arg4 == '789'
+
+	def test_inheritance(self):
+		obj = InheritTestClass(1, 2, 3, 4)
+		assert obj.arg1 == 1
+		self.assertNotExist(lambda: obj.arg1_ == 1)
+
+		self.assertNotExist(lambda: obj.arg2_ == 2)
+		assert obj.arg2 == 2
+
+		assert obj._newarg == 3
+		self.assertNotExist(lambda: obj.arg3_ == 3)
+
+		self.assertNotExist(lambda: obj.arg4_ == '123')
+		assert obj._arg4 == '123'
+
+	def test_nodefaults(self):
+		obj = NoDefaultsTestClass(1, 2, 3, 4)
+		self.assertNotExist(lambda: obj.arg1_ == 1)
+		assert obj.arg2 == 2
+		assert obj._arg3 == 3
+		assert obj.arg4 == 4
+
+if __name__ == '__main__':
+	unittest.main()
